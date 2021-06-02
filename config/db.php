941c1e53e72b54961af6128e994f83c1b272c809
Ruslan_Kushnir <ruslan.kushnir.2010@gmail.com>
2021-06-02 12:21:06 +0300

    Настроил работу mysql в докере.

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=',
-    'username' => '',
-    'password' => '',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=test-covid.loc',
+    'username' => 'test-covid',
+    'password' => '!Test-Covid_1',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
